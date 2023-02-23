55263bfd4929909cf713ef8bdbef3879396c1c40
Bryan Luna <bryan_gol_12@hotmail.es>
2023-02-23 14:29:21 -0500

    0.01

--- Video.py
+++ Video.py
@@ -5,8 +5,8 @@ cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Variable para controlar el tiempo de toma de fotos
-start_time = time.time()
+# Variable para controlar la toma de fotos
+take_photo = False
 
 while True:
     ret, frame = cap.read()
@@ -15,6 +15,9 @@ while True:
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+    # Inicializar roi_color fuera del bucle for
+    roi_color = None
+
     for (x, y, w, h) in faces:
 
         # Recortar la región del rostro dentro del rectángulo verde
@@ -22,13 +25,16 @@ while True:
 
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        # Tomar una foto de la región del rostro sin los bordes del rectángulo
-        if time.time() - start_time >= 5:
-            img_name = "foto_{}.png".format(str(time.time()).replace(".", ""))
-            cv2.imwrite(img_name, roi_color)
-            print("Foto guardada como: ", img_name)
+    # Verificar si se presiona la tecla 'c'
+    if cv2.waitKey(1) == ord('c'):
+        take_photo = True
 
-            start_time = time.time()
+    # Tomar una foto de la región del rostro sin los bordes del rectángulo
+    if take_photo and roi_color is not None: # Verificar si se ha detectado algún rostro
+        img_name = "foto_{}.png".format(str(time.time()).replace(".", ""))
+        cv2.imwrite(img_name, roi_color)
+        print("Foto guardada como: ", img_name)
+        take_photo = False
 
     frame = cv2.flip(frame, 1)
     cv2.imshow('frame', frame)
